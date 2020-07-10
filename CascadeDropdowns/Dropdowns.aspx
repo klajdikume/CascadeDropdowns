ee8f35bff26a05b8865af59eeb8446ca03ffbdd5
klajdikume <klajdi.kume@fshnstudent.info>
2020-07-10 17:10:50 +0200

    first commit

---
+++ CascadeDropdowns/Dropdowns.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Dropdowns.aspx.cs" Inherits="CascadeDropdowns.Dropdowns" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="ddlContinents" runat="server" Height="24px" Width="145px"
+                DataTextField="ContinentName" DataValueField="ContinentId" 
+                OnSelectedIndexChanged="ddlContinents_SelectedIndexChanged"
+                AutoPostBack="true">
+            </asp:DropDownList>
+            <br />
+            <br />
+        </div>
+        <asp:DropDownList ID="ddlCountries" runat="server" Height="18px" Width="147px"
+            DataTextField="CountryName" DataValueField="CountryId" 
+            OnSelectedIndexChanged="ddlCountries_SelectedIndexChanged"
+            AutoPostBack="true">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:DropDownList ID="ddlCities" runat="server" Height="16px" Width="149px" DataTextField="CityName" DataValueField="CityId">
+        </asp:DropDownList>
+    </form>
+</body>
+</html>
---
+++ CascadeDropdowns/Dropdowns.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CascadeDropdowns
+{
+    public partial class Dropdowns : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //initial get
+                ddlContinents.DataSource = GetData("spGetContinents", null);
+                ddlContinents.DataBind();
+
+                ListItem liContinent = new ListItem("Select Continent","-1");
+                ddlContinents.Items.Insert(0, liContinent);
+
+                ListItem liCountry = new ListItem("Select Country", "-1");
+                ddlCountries.Items.Insert(0, liCountry);
+
+                ListItem liCity = new ListItem("Select City", "-1");
+                ddlCities.Items.Insert(0, liCity);
+
+                ddlCountries.Enabled = false;
+                ddlCities.Enabled = false;
+            }
+        }
+
+        private DataSet GetData(string SPName, SqlParameter SPParameter)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+
+            SqlDataAdapter da = new SqlDataAdapter(SPName, con);
+            
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            
+            if(SPParameter != null)
+            {
+                da.SelectCommand.Parameters.Add(SPParameter);
+            }
+
+            DataSet dataSet = new DataSet();
+            da.Fill(dataSet);
+
+            return dataSet;
+        }
+
+        protected void ddlContinents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(ddlContinents.SelectedIndex == 0)
+            {
+                //ddlCountries.SelectedIndex = 0;
+                //ddlCountries.Enabled = false;
+
+                ddlCities.SelectedIndex = 0;
+                ddlCities.Enabled = false;
+            }
+            else
+            {
+                ddlCountries.Enabled = true;
+                SqlParameter parameter = new SqlParameter("@ContinentId", ddlContinents.SelectedValue);
+                DataSet ds = GetData("spGetCountriesByContinentId", parameter);
+
+                ddlCountries.DataSource = ds;
+                ddlCountries.DataBind();
+
+                ListItem liCountry = new ListItem("Select Country", "-1");
+                ddlCountries.Items.Insert(0, liCountry);
+
+                ddlCities.SelectedIndex = 0;
+                ddlCities.Enabled = false;
+            }
+        }
+
+        protected void ddlCountries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlCities.SelectedIndex == 0)
+            {
+
+            }
+            else
+            {
+                ddlCities.Enabled = true;
+                SqlParameter parameter = new SqlParameter("@CountryId", ddlCountries.SelectedValue);
+                DataSet ds = GetData("spGetCitiesByCountryId", parameter);
+
+                ddlCities.DataSource = ds;
+                ddlCities.DataBind();
+
+                ListItem liCity = new ListItem("Select City", "-1");
+                ddlCities.Items.Insert(0, liCity);
+            }
+        }
+    }
+}
